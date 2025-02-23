d4eb28c10b87ec118f16051a7ffe8adf35b8e3ef
ASH <ayushsinghd49@gmail.com>
2025-02-23 16:39:49 +0530

    done

--- Sborn.py
+++ Sborn.py
@@ -10,3 +10,21 @@ crash_df = sns.load_dataset('car_crashes')
 print(crash_df.head())
 sns.jointplot(x='speeding',y= 'alcohol',data =crash_df,kind='resid')
 plt.show()
+#Joint Plot
+
+#plot_kinds = ["scatter", "hist", "hex", "kde", "reg", "resid"]
+sns.jointplot(x='speeding',y= 'alcohol',data =crash_df,kind='resid')
+
+#KDE PLOT
+sns.kdeplot(crash_df['alcohol'])
+
+# Pair Plot
+#sns.pairplot(crash_df)
+tips_df= sns.load_dataset('tips')
+#tips_df.plot()
+sns.pairplot(tips_df,hue='sex',palette='Reds')
+
+#Rug Plot
+tips_df= sns.load_dataset('tips')
+sns.rugplot(tips_df['total_bill'])
+
